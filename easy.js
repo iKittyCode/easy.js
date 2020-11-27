7cae785829aada230bc9e7c7b15e3a4bce7e770b
iKittyCode <gator.syon@gmail.com>
2020-11-27 18:26:02 -0500

    Orginal commit

---
+++ easy.js
@@ -0,0 +1,21 @@
+// MIT license by coding can be easy foundation
+class Easy {
+
+    print(message) {
+        console.log(message)
+    }
+    popUpMessage(messsage) {
+        alert(messsage)
+    }
+    createAParagrph(text) {
+        document.createElement('p').textContent = text
+    }
+    inputPopUp(message) {
+        prompt(message)
+    }
+    Div(insider) {
+    document.createElement('div').innerHTML = insider
+    }
+    
+
+}
